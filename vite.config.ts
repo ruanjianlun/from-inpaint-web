fac382df45db2076a261f01404fb42c2fd85e6a9
TomLbj <zhaozed888@gmail.com>
2026-01-13 23:57:22 +0800

    docker err

--- vite.config.ts
+++ vite.config.ts
@@ -24,4 +24,16 @@ export default defineConfig({
       'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
+  build: {
+    rollupOptions: {
+      external: id => {
+        // 不要将 ort 视为外部模块（它通过 CDN 动态加载）
+        return false
+      },
+      output: {
+        // 确保 ort 被正确引用为全局变量
+        globals: {},
+      },
+    },
+  },
 })
